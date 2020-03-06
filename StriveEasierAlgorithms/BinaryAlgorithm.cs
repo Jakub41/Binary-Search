af7209563a7f43575ed3beefd567bb32e1a64ca0
Jakub <lemiszewski@gmx.com>
2020-03-06 14:03:57 +0100

    Added solution for the Binary Search Assigment

---
+++ StriveEasierAlgorithms/BinaryAlgorithm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StriveEasierAlgorithms
+{
+   public class BinaryAlgorithm
+    {
+        public static List<int> GetBinaryList()
+        {
+            List<int> collectionToSearchIn = new List<int>() { 5, 8, 9, 11, 12, 13, 15, 18, 19, 24, 25, 27, 29, 34, 35, 39 };
+            return collectionToSearchIn;
+        }
+    }
+}
